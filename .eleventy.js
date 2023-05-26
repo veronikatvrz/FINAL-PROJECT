5642202a3ff6f011da678330e83780f1f6c8699a
Veronika Tvrzníková <tvrznikova.v@gmail.com>
2023-05-26 11:55:06 +0200

    formatting done

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("icons");
+    eleventyConfig.addPassthroughCopy("css");
+    
+
   
 module.exports = function(eleventyConfig) {
     return {
